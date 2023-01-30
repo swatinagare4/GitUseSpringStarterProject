434ebbc41ac4a4fbe75c0a4127c6129029d972d8
swatinagare4 <n.swati111@gmail.com>
2023-01-30 16:08:47 +0530

    changes by swati

---
+++ src/main/java/com/Swati/GitUseApplication.java
@@ -0,0 +1,13 @@
+package com.Swati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Swati/GitUseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Swati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitUseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
